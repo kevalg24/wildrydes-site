8a25e848efa935a13bcdfb870a5f6ef0cbbb5c2d
Keval <keval.g24@gmail.com>
2021-04-25 10:44:44 +0000

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_pOy1k4Og0
+        userPoolClientId: '', // e.g. 10qo74g18os8qjcvn2grf8vet1
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
